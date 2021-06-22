c761383335bf0c284435d3c598c72d59a99df795
Vividh Bhardwaj <bhardwajvividh@gmail.com>
2021-06-22 23:32:15 +0530

    Fixed typos, enhanced code and removed dead code

--- acceptance/new-user/sign-up.test.js
+++ acceptance/new-user/sign-up.test.js
@@ -4,105 +4,98 @@ const config = require("config");
 const URLS = require("../../constants/urls");
 
 const { HOME_PAGE, SIGN_UP_PAGE, MY_HOST } = URLS;
+const {signupPageTitle} = require("../../constants/pageTitles")
+
 
 let browser, page;
 
-function delay(time) {
-    return new Promise(function (res) {
-        setTimeout(res, time);
-    });
-}
+const delay = (time) => {
+  return new Promise((res) => {
+    setTimeout(res, time);
+  });
+};
 
 beforeAll(async () => {
-    browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 100,
-    });
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 0,
+  });
 
-    const context = await browser.createIncognitoBrowserContext();
+  const context = await browser.createIncognitoBrowserContext();
 
-    page = await context.newPage();
-    return page;
+  page = await context.newPage();
+  return page;
 });
 
 afterAll(async () => {
-    await browser.close();
+  await browser.close();
 });
 
-describe("New user navigates", () => {
-    test("New user sess a sign up page", async () => {
-        await page.goto(HOME_PAGE);
+describe("New user navigates in the page", () => {
+  test("New user sees a sign up page", async () => {
+    await page.goto(HOME_PAGE);
 
-        await page.waitForResponse((res) => res.url().endsWith("/users/self"));
-        await page.waitForSelector("button.login-btn-text");
+    await page.waitForResponse((res) => res.url().endsWith("/users/self"));
+    await page.waitForSelector("button.login-btn-text");
 
-        await Promise.all([
-            page.waitForNavigation(),
-            page.click("button.login-btn-text"),
-        ]);
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click("button.login-btn-text"),
+    ]);
 
-        const ghUsernameInput = await page.waitForSelector("input#login_field");
-        const ghPasswordInput = await page.waitForSelector("input#password");
+    const ghUsernameInput = await page.waitForSelector("input#login_field");
+    const ghPasswordInput = await page.waitForSelector("input#password");
 
-        await ghUsernameInput.type(config.get("testUser.username"));
-        await ghPasswordInput.type(config.get("testUser.password"));
-        await Promise.all([
-            page.waitForNavigation(),
-            page.keyboard.press("Enter"),
-        ]);
-        await page.waitForSelector("button#js-oauth-authorize-btn");
-        await delay(2000);
-        await page.click("button#js-oauth-authorize-btn");
-        await page.waitForNavigation();
-        await page.waitForFunction(
-            `window.location.href.includes("${SIGN_UP_PAGE}")`
-        );
-        await page.waitForSelector("button.submitButton");
-        console.log("submit button found");
-        await page.screenshot({ path: "tmp/sign-up-form.png" });
-        console.log("Screenshot taken");
-        const pageTitle = await page.title();
-        console.log(pageTitle);
-        expect(pageTitle).toMatch(/Sign Up/);
-    });
+    await ghUsernameInput.type(config.get("testUser.username"));
+    await ghPasswordInput.type(config.get("testUser.password"));
+    await Promise.all([page.waitForNavigation(), page.keyboard.press("Enter")]);
+    await page.waitForSelector("button#js-oauth-authorize-btn");
+    await delay(2000);
+    await page.click("button#js-oauth-authorize-btn");
+    await page.waitForNavigation();
+    await page.waitForFunction(
+      `window.location.href.includes("${SIGN_UP_PAGE}")`
+    );
+    await page.waitForSelector("button.submitButton");
+    await page.screenshot({ path: "tmp/sign-up-form.png" });
+    const pageTitle = await page.title();
+    expect(pageTitle).toMatch(signupPageTitle);
+  });
 });
 
 describe("New user sign up page works correctly", () => {
-    beforeEach(async () => {
-        await page.setRequestInterception(true);
-    });
+  beforeEach(async () => {
+    await page.setRequestInterception(true);
+  });
 
-    afterEach(async () => {
-        await page.setRequestInterception(false);
-    });
+  afterEach(async () => {
+    await page.setRequestInterception(false);
+  });
 
-    test("Incomplete user sees sign up page", async () => {
-        page.on("request", (request) => {
-            if (
-                request.url().endsWith("/users/self") &&
-                request.method() === "GET"
-            ) {
-                request.respond({
-                    headers: {
-                        "Access-Control-Allow-Credentials": true,
-                        "Access-Control-Allow-Origin": MY_HOST,
-                    },
-                    body: JSON.stringify({
-                        id: "<>",
-                        incompleteUserDetails: true,
-                        github_display_name: "<>",
-                        github_id: "<>",
-                    }),
-                });
-            } else {
-                request.continue();
-            }
+  test("Incomplete user sees sign up page", async () => {
+    page.on("request", (request) => {
+      if (request.url().endsWith("/users/self") && request.method() === "GET") {
+        request.respond({
+          headers: {
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Origin": MY_HOST,
+          },
+          body: JSON.stringify({
+            id: "<>",
+            incompleteUserDetails: true,
+            github_display_name: "<>",
+            github_id: "<>",
+          }),
         });
-
-        await page.goto(SIGN_UP_PAGE);
-        await page.waitForSelector("h2");
-
-        const pageTitle = await page.title();
-        expect(pageTitle).toMatch(/Sign Up/);
+      } else {
+        request.continue();
+      }
     });
+
+    await page.goto(SIGN_UP_PAGE);
+    await page.waitForSelector("h2");
+
+    const pageTitle = await page.title();
+    expect(pageTitle).toMatch(signupPageTitle);
+  });
 });
--- visitor-tests/home-page.test.js
+++ visitor-tests/home-page.test.js
@@ -7,43 +7,67 @@ const { HOME_PAGE } = URLS;
 let browser, page;
 
 beforeAll(async () => {
-    browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 100,
-    });
-    const context = await browser.createIncognitoBrowserContext();
-    page = await context.newPage();
-    return page;
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 100,
+  });
+  const context = await browser.createIncognitoBrowserContext();
+  page = await context.newPage();
+  return page;
 });
 
 afterAll(async () => {
-    await browser.close();
+  await browser.close();
+});
+
+test("Text checks", async () => {
+  await page.goto(HOME_PAGE);
+  await page.waitForSelector(".main-banner");
+  const bannerText = await page.$eval(".notif", (el) => el.innerText);
+  expect(bannerText).toMatch(
+    "New 1 year Anniversary -- New features available for you Check them out!"
+  );
+
+  await page.waitForSelector(".welcome-title");
+  const titleText = await page.$eval(".welcome-title", (el) => el.innerText);
+  expect(titleText).toMatch("Welcome to Real Dev Squad");
+
+  await page.waitForSelector(".video-text");
+  const videoText = await page.$eval(".video-text", (el) => el.innerText);
+  expect(videoText).toMatch("Check out what Real Dev Squad is");
+
+  await page.waitForSelector(".next-event-header");
+  const checkEventsText = await page.$eval(
+    ".next-event-header",
+    (el) => el.innerText
+  );
+  expect(checkEventsText).toMatch("You can check out all our events below!!");
 });
 
 describe("Click on Social Media Links", () => {
-    test("Visitor click on social media links", async () => {
-        await page.goto(HOME_PAGE);
-        const links = await page.$$eval("a.social-media-link", (list) =>
-            list.map((elm) => elm.href)
-        );
-        for (const link of links) {
-            const newPage = await browser.newPage();
-            await newPage.goto(link);
-            if (link.includes("linkedin")) {
-                await newPage.screenshot({ path: "social-media/linkedin.png" });
-                expect(link).toMatch(/real-dev-squad/);
-            } else if (link.includes("twitter")) {
-                await newPage.screenshot({ path: "social-media/twitter.png" });
-                expect(link).toMatch(/realdevsquad/);
-            } else if (link.includes("facebook")) {
-                await newPage.screenshot({ path: "social-media/facebook.png" });
-                expect(link).toMatch(/Real-Dev-Squad/);
-            } else {
-                await newPage.screenshot({
-                    path: "social-media/instagram.png",
-                });
-                expect(link).toMatch(/realdevsquad/);
-            }
-        }
-    });
+  test("Visitor click on social media links", async () => {
+    await page.goto(HOME_PAGE);
+    const links = await page.$$eval("a.social-media-link", (list) =>
+      list.map((elm) => elm.href)
+    );
+    for (const link of links) {
+      const newPage = await browser.newPage();
+      await newPage.goto(link);
+      if (link.includes("linkedin")) {
+        await newPage.screenshot({ path: "social-media/linkedin.png" });
+        expect(link).toMatch(/real-dev-squad/);
+      } else if (link.includes("twitter")) {
+        await newPage.screenshot({ path: "social-media/twitter.png" });
+        expect(link).toMatch(/realdevsquad/);
+      } else if (link.includes("facebook")) {
+        await newPage.screenshot({ path: "social-media/facebook.png" });
+        expect(link).toMatch(/Real-Dev-Squad/);
+      } else {
+        await newPage.screenshot({
+          path: "social-media/instagram.png",
+        });
+        expect(link).toMatch(/realdevsquad/);
+      }
+    }
+  });
 });
