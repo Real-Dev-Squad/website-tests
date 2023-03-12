ada05cc3d0a9f14923359d2c828e8b48988e4e3c
ankushdharkar <ankushdharkar@gmail.com>
2023-03-11 17:30:55 -0800

    Got our website tests and interactions working

--- acceptance/new-user/www-loads.test.ts
+++ acceptance/new-user/www-loads.test.ts
@@ -17,8 +17,15 @@ beforeAll(async () => {
 
 describe("Homepage", () => {
   beforeEach(async () => {
-    page_x = await context_x.newPage(); // await page , which is slower
-    await page_x.goto("https://google.com");
+    page_x = await context_x.newPage(); // better than the slower `await page`
+
+    await Promise.all([
+      page_x.waitForResponse((res) => res.url().endsWith("/users/self")),
+      page_x.goto("https://realdevsquad.com", { // https://pptr.dev/api/puppeteer.page.goto/
+        waitUntil: 'networkidle0'
+      }),
+      // page_x.waitForNavigation(),
+    ]);
   });
 
   afterEach(async () => {
@@ -26,7 +33,27 @@ describe("Homepage", () => {
   });
 
 
-  it('should display "Google" text on page', async () => {
-    await expect(page_x).toMatch("google");
+  it('should display "Real Dev Squad" text on page in desktop', async () => {
+    await expect(page_x).toMatch('Real Dev Squad');
+  });
+
+  it('should display "Real Dev Squad" text on page in mobile', async () => {
+    await page_x.setViewport({
+      width: 640,
+      height: 480,
+      deviceScaleFactor: 1,
+    });
+      
+    await expect(page_x).toMatch('Real Dev Squad');
+
+    // Won't work, because we don't have this visible in mobile:
+    // await page_x.click("body > nav > ul > li:nth-child(3) > a");
+  });
+
+  it('should visit Welcome site', async () => {
+    // await page_x.waitForSelector('li > a');
+    await expect(page_x).toMatchElement('body > nav > ul > li:nth-child(3) > a');
+    // works too: await expect(page_x).toClick("body > nav > ul > li:nth-child(3) > a");
+    await page_x.click("body > nav > ul > li:nth-child(3) > a");
   });
 });
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,6 +4,10 @@ module.exports = {
     headless: process.env.HEADLESS !== "false",
     product: "chrome",
     // slowMo: 300,
+    defaultViewport: {
+      width: 1920,
+      height: 1080
+    }
   },
   browserContext: "default",
 };
