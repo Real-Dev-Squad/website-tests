67ac12e643f8f1ba80e7fccf177568bf9e5deaad
Khushi Shukla <khushishukla5890@gmail.com>
2023-03-25 00:02:18 +0530

    sign up navigation

--- acceptance/new-user/sign-up-flow.test.js
+++ acceptance/new-user/sign-up-flow.test.js
@@ -3,7 +3,7 @@ const config = require("config");
 
 const URLS = require("../../constants/urls");
 
-const { HOME_PAGE, SIGN_UP_PAGE, MY_HOST } = URLS;
+const { HOME_PAGE, SIGN_UP_PAGE, MY_HOST , SELF_USER_API} = URLS;
 const {signupPageTitle} = require("../../constants/pageTitles")
 
 beforeAll(async () => {
@@ -19,46 +19,85 @@ beforeAll(async () => {
 });
 
 
-// afterAll(async () => {
-//   await browser.close();
-// });
+  
 
-describe("New user navigates in the page", () => {
-  test("github signin", async () => {
+test('can sign in and authorize with GitHub', async () => {
     await page.goto(HOME_PAGE);
-
     await Promise.all([
       page.waitForNavigation(),
       page.click("a.btn-login"),
     ]);
 
-    const ghUsernameInput = await page.waitForSelector("input#login_field");
-    const ghPasswordInput = await page.waitForSelector("input#password");
+    // Fill in the test account's login credentials and submit the login form
+    await page.type('#login_field', config.get("testUser.username"));
+    await page.type('#password', config.get("testUser.password"));
+    await page.click('[type="submit"]');
 
-    if(ghPasswordInput && ghUsernameInput)
-    {
-      await ghUsernameInput.type(config.get("testUser.username"));
-      await ghPasswordInput.type(config.get("testUser.password"));
-      await Promise.all([page.waitForNavigation(), page.keyboard.press("Enter")]);
-      
-    //   const pageTitle = await page.$eval(document.title) 
-    //   console.log(pageTitle)
-    // //    Authorization applition
-    //   await delay(2000)
-     
-    //   await page.waitForSelector("button#js-oauth-authorize-btn");
-    //   await Promise.all([
-    //   await page.click("button#js-oauth-authorize-btn"),
-    //   await page.waitForNavigation()
-    //   ])
-    }
-    
+    // Wait for the GitHub authorization page to load
+    await page.waitForSelector('#js-oauth-authorize-btn');
+
+    // Click the "Authorize" button to authorize the application
     await Promise.all([
-        page.waitForNavigation(), 
-        page.keyboard.press("Enter")
-    ]);
+      await page.click("button#js-oauth-authorize-btn"),
+      await page.waitForNavigation()
+    ])
+
+    /* navigate to signup */
+    // await page.setRequestInterception(true);
+    // page.on("request", (request) => {
+        
+    //   if (request.url().endsWith("/users/self") && request.method() === "GET") {
+        
+    //     request.respond({
+    //       headers: {
+    //         "Access-Control-Allow-Credentials": true,
+    //         "Access-Control-Allow-Origin": MY_HOST,
+    //       },
+    //       body: JSON.stringify({
+    //         id: "<>",
+    //         incompleteUserDetails: true,
+    //         github_display_name: "<>",
+    //         github_id: "<>",
+    //       }),
+    //     });
+    //   } else {
+    //     request.continue();
+    //   }
+    // });
+    //   await page.goto(SIGN_UP_PAGE)
+      
+      browser.close()    
+  });
+
+describe("New user sign up page works correctly",() => {
+   test("Incomplete user sees sign up page", async () => {
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (request.url().endsWith("/users/self") && request.method() === "GET") {
+        request.respond({
+          headers: {
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Origin": MY_HOST,
+          },
+          body: JSON.stringify({
+            id: "<>",
+            incompleteUserDetails: true,
+            github_display_name: "<>",
+            github_id: "<>",
+          }),
+        });
+      } else {
+        request.continue();
+      }
+    });
+     await page.goto(SIGN_UP_PAGE);
+
+     browser.close()
   })
-})    
+})
+    
+  
+
 
 
 
