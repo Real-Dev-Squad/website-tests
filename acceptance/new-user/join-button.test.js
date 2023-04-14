07ad56f6851222e8862458f59fac831e1c3cfc03
Khushi Shukla <khushishukla5890@gmail.com>
2023-04-14 18:44:43 +0530

    fix/tests-style

--- acceptance/new-user/join-button.test.js
+++ acceptance/new-user/join-button.test.js
@@ -2,11 +2,9 @@ const puppeteer = require("puppeteer");
 
 const URLS = require("../../constants/urls");
 
-const {JOIN } = URLS;
-
+const { JOIN } = URLS;
 
 let browser, page;
-
 
 /* time delay */
 const delay = (time) => {
@@ -16,36 +14,38 @@ const delay = (time) => {
 };
 
 /* confirm alerts */
-async function confirmAlerts(){
-        page.on('dialog', async dialog => {
-               await dialog.accept();
-               
-               delay(2000)
-               await page.waitForNavigation()
+async function confirmAlerts() {
+  page.on("dialog", async (dialog) => {
+    await dialog.accept();
 
-               delay(4000)
-               await browser.close()
-        })
+    delay(2000);
+    await page.waitForNavigation();
+
+    delay(4000);
+    await browser.close();
+  });
 }
 
-async function checkResponse(){
- page.on("response", (response) => {            
-    if(response.url().endsWith("/users/self") && page.url().endsWith('/join')){
-        switch (response.status()) {
-	           	case 401:
-			            delay(2000);
-			            confirmAlerts();
-			            break;
-		          case 200:
-			            browser.close();
-			            break;
-              default:
-                   browser.close() 
-	          }
-          }
- })
+async function checkResponse() {
+  page.on("response", (response) => {
+    if (
+      response.url().endsWith("/users/self") &&
+      page.url().endsWith("/join")
+    ) {
+      switch (response.status()) {
+        case 401:
+          delay(2000);
+          confirmAlerts();
+          break;
+        case 200:
+          browser.close();
+          break;
+        default:
+          browser.close();
+      }
+    }
+  });
 }
- 
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
@@ -61,15 +61,10 @@ beforeAll(async () => {
 
 describe("Navigation of Join button with logged user and logged out user", () => {
   test("Join button test for user", async () => {
+    await page.goto(JOIN);
 
-    await page.goto(JOIN)
-     
-    await Promise.all([
-            page.click('.btn-join'), 
-            page.waitForNavigation() 
-    ]);
-         
-    delay(2000)
-    await checkResponse()        
-  })
-})
+    await Promise.all([page.click(".btn-join"), page.waitForNavigation()]);
+
+    await checkResponse();
+  });
+});
